2beee81e05c6831d5fb6049f086799e86d004d73
mihir1910 <89451905+mihir1910@users.noreply.github.com>
2022-08-28 10:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import requests
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/65?api_key=8f22d5686b436b05ef836075c657fdd6&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
